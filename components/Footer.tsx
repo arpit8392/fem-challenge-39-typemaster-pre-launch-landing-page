90772359df0268124cb5353dfde393cfd3af46ab
Arpit Namdev <arpit8392@gmail.com>
2023-07-28 12:08:07 +0530

    Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,11 @@
 const Footer = () => {
-	return <div>Footer</div>
+	return (
+		<footer className='py-14 text-center md:py-11 lg:py-10'>
+			<p className='text-pastelBlue'>
+				<span className='font-medium'>Typemaster 2021 &nbsp; | &nbsp; </span>All
+				Rights Reserved
+			</p>
+		</footer>
+	)
 }
 export default Footer
